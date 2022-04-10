b9f2c0fab0f5363e7d070a3d4d46d253d3940533
Timo Prüße <chroma91@gmail.com>
2022-04-10 12:43:00 +0200

    alias for running machine_setup from anywhere

--- home/.oh-my-zsh/custom/system_aliases.zsh
+++ home/.oh-my-zsh/custom/system_aliases.zsh
@@ -2,3 +2,4 @@ alias upgrade="sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y"
 alias nvim=/home/timo/.nvim/nvim.appimage
 alias vim=nvim
 alias python=python3
+alias ms="machine_setup -c ~/github/Chroma91/.dotfiles/machine_setup.yaml"
