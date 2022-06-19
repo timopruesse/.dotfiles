9833344423738b2e2017bbd1002e088f247769fd
Timo Prüße <chroma91@gmail.com>
2022-06-19 03:10:33 +0200

    remove now unneeeded keymaps

--- home/.config/nvim/lua/timopruesse/keymaps/git.lua
+++ home/.config/nvim/lua/timopruesse/keymaps/git.lua
@@ -1,16 +1,8 @@
 local key = require("timopruesse.helpers.keymap")
 local neogit = require("neogit")
 
-key.nnoremap("<leader>ga", key.exec_command(":Git fetch --all"))
-key.nnoremap("<leader>grum", key.exec_command(":Git rebase upstream/main"))
-key.nnoremap("<leader>grom", key.exec_command(":Git rebase origin/main"))
-
 local open = function()
 	neogit.open({ kind = "split" })
 end
 
 key.nmap("<leader>gg", open)
-key.nmap("<leader>gh", key.exec_command(":diffget //3"))
-key.nmap("<leader>gf", key.exec_command(":diffget //2"))
-key.nmap("<leader>gc", key.exec_command(":G commit"))
-key.nmap("<leader>gp", key.exec_command(":G push"))
