661efea1ce5bf30d6e846daf55e34dc39fc4d7da
Timo Prüße <chroma91@gmail.com>
2022-06-23 22:05:43 +0200

    remove autocommand for showing diagnostics

--- home/.config/nvim/lua/timopruesse/autocommands/init.lua
+++ home/.config/nvim/lua/timopruesse/autocommands/init.lua
@@ -1,28 +1,15 @@
-local show_diagnostics = function()
-	vim.diagnostic.open_float(nil, { focusable = false })
-end
-
 local show_highlight = function()
 	require("vim.highlight").on_yank({ timeout = 1000 })
 end
 
-local au_tpr = vim.api.nvim_create_augroup("TIMOPRUESSE", {})
 local au_yank = vim.api.nvim_create_augroup("highlight_yank", {})
 local au_fmt = vim.api.nvim_create_augroup("fmt", {})
 
-vim.api.nvim_create_autocmd({ "CursorHold" }, {
-	pattern =  {"*"},
-	callback = show_diagnostics,
- group = au_tpr })
-
-vim.api.nvim_create_autocmd(
-	{ "TextYankPost" },
-	{ pattern = {"*"}, callback = show_highlight , group = au_yank }
-)
+vim.api.nvim_create_autocmd({ "TextYankPost" }, { pattern = { "*" }, callback = show_highlight, group = au_yank })
 
 vim.api.nvim_create_autocmd(
 	{ "BufWritePre" },
-	{ pattern = {"*"}, command = "try | undojoin | Neoformat | catch /E790/ | Neoformat | endtry" , group = au_fmt }
+	{ pattern = { "*" }, command = "try | undojoin | Neoformat | catch /E790/ | Neoformat | endtry", group = au_fmt }
 )
 
 require("timopruesse.autocommands.yaml")
