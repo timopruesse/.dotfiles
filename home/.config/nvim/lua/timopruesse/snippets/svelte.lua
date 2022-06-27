4b118ea46420376f96c4c184bf782a0b87db3b1f
Timo Prüße <chroma91@gmail.com>
2022-06-27 11:33:40 +0200

    better svelte snippet for script tags

--- home/.config/nvim/lua/timopruesse/snippets/svelte.lua
+++ home/.config/nvim/lua/timopruesse/snippets/svelte.lua
@@ -1,6 +1,24 @@
 local ls = require("luasnip")
+local fmt = require("luasnip.extras.fmt").fmt
+
+local s = ls.s
+local i = ls.insert_node
+local c = ls.choice_node
+local t = ls.text_node
 
 ls.add_snippets("svelte", {
-	ls.parser.parse_snippet("scr", '<script lang="ts">\n$0\n</script>'),
-	ls.parser.parse_snippet("mod", '<script context="module">\n$0\n</script>'),
+	s(
+		"script",
+		fmt(
+			[[
+                <script {}>
+                    {}
+                </script>
+            ]],
+			{
+				c(1, { t('lang="ts"'), t('context="module"') }),
+				i(0),
+			}
+		)
+	),
 })
