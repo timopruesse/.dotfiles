efe97f83ac1fe01640ab843563976af7c81db53e
Timo Prüße <chroma91@gmail.com>
2022-08-09 14:48:32 +0200

    remove unneeded local variable

--- home/.config/nvim/lua/timopruesse/keymaps/rust.lua
+++ home/.config/nvim/lua/timopruesse/keymaps/rust.lua
@@ -6,9 +6,7 @@ M.setup = function(bufnr)
 	local buffer = bufnr or false
 
 	key.nnoremap("<leader>rr", require("rust-tools.runnables").runnables, buffer)
-
-	local rt = require("rust-tools")
-	key.nmap("<C-Space>", rt.hover_actions.hover_actions, buffer)
+	key.nmap("<C-Space>", require("rust-tools").hover_actions.hover_actions, buffer)
 end
 
 return M
