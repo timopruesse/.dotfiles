5f61b6153104fc0add1193cc66040af74f3ec25e
Timo Prüße <chroma91@gmail.com>
2022-08-15 23:48:30 +0200

    better tmux aliases

--- home/.oh-my-zsh/custom/system_aliases.zsh
+++ home/.oh-my-zsh/custom/system_aliases.zsh
@@ -7,4 +7,10 @@ alias python=python3
 
 alias ms="machine_setup -c $DOTFILES/machine_setup.yaml"
 
-alias start="tmux new -stimo"
+export TMUX_SESSION="timo"
+
+alias tmux_create="tmux new -s$TMUX_SESSION"
+alias tmux_attach="tmux attach -d -t $TMUX_SESSION"
+
+alias start="tmux_attach || tmux_create"
+alias tkill="tmux kill-session -t $TMUX_SESSION"
