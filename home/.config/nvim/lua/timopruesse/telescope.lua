5ac1b15753cff9b4a5d4a8f6add788dc6fb4b5b1
Timo Prüße <chroma91@gmail.com>
2022-09-30 11:46:01 +0200

    find "all" files

--- home/.config/nvim/lua/timopruesse/telescope.lua
+++ home/.config/nvim/lua/timopruesse/telescope.lua
@@ -68,10 +68,7 @@ local M = {}
 M.project_files = function()
 	local opts = { hidden = true }
 
-	local ok = pcall(require("telescope.builtin").git_files, opts)
-	if not ok then
-		require("telescope.builtin").find_files(opts)
-	end
+	require("telescope.builtin").find_files(opts)
 end
 
 M.search_dotfiles = function()
