d6932600d242642b1edcac812ad955c7528e7b4a
Timo Prüße <chroma91@gmail.com>
2023-08-01 22:27:46 +0200

    don't error when no LSP is attached

--- home/.config/nvim/lua/timopruesse/autocommands/init.lua
+++ home/.config/nvim/lua/timopruesse/autocommands/init.lua
@@ -1,49 +1,49 @@
 local au_yank = vim.api.nvim_create_augroup("highlight_yank", { clear = true })
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
-	pattern = { "*" },
-	callback = function()
-		require("vim.highlight").on_yank({ timeout = 800 })
-	end,
-	group = au_yank,
+    pattern = { "*" },
+    callback = function()
+        require("vim.highlight").on_yank({ timeout = 800 })
+    end,
+    group = au_yank,
 })
 
 local au_fmt = vim.api.nvim_create_augroup("fmt", { clear = true })
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-	pattern = { "*" },
-	callback = function()
-		vim.lsp.buf.format()
-	end,
-	group = au_fmt,
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+    pattern = { "*" },
+    callback = function()
+        vim.lsp.buf.format()
+    end,
+    group = au_fmt,
 })
 
 local toggle_markdown_preview = function()
-	local peek = require("peek")
+    local peek = require("peek")
 
-	if peek.is_open() then
-		peek.close()
-	else
-		peek.open()
-	end
+    if peek.is_open() then
+        peek.close()
+    else
+        peek.open()
+    end
 end
 
 local preview_markdown = function(cmd)
-	local key = require("timopruesse.helpers.keymap")
+    local key = require("timopruesse.helpers.keymap")
 
-	local keys = "<leader>md"
-	if cmd.event == "BufEnter" then
-		key.nnoremap(keys, toggle_markdown_preview, cmd.buf)
-	else
-		key.nremovemap(keys, cmd.buf)
-	end
+    local keys = "<leader>md"
+    if cmd.event == "BufEnter" then
+        key.nnoremap(keys, toggle_markdown_preview, cmd.buf)
+    else
+        key.nremovemap(keys, cmd.buf)
+    end
 end
 
 local au_md = vim.api.nvim_create_augroup("markdown", { clear = true })
 vim.api.nvim_create_autocmd({ "BufEnter", "BufLeave" }, {
-	pattern = { "*.md" },
-	callback = function(cmd)
-		preview_markdown(cmd)
-	end,
-	group = au_md,
+    pattern = { "*.md" },
+    callback = function(cmd)
+        preview_markdown(cmd)
+    end,
+    group = au_md,
 })
 
 require("timopruesse.autocommands.inlay_hints")
