5e13154b4b497ce9f109aee6e881b3ddc52c97ad
Timo Prüße <chroma91@gmail.com>
2024-09-28 17:15:29 +0200

    fix: create a new session if inside Tmux and the session doesn't exist

--- home/start_tmux.sh
+++ home/start_tmux.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-  echo "no session name provided"
-  exit 1
+    echo "No session name provided"
+    exit 1
 fi
+
+create_or_switch_session() {
+    if ! tmux has-session -t $1 2>/dev/null; then
+        tmux new-session -d -s $1
+    fi
+    tmux switch-client -t $1
+}
 
 if [ -n "$TMUX" ]; then
-  tmux switch -t $1
-  exit 0
+    create_or_switch_session $1
+else
+    tmux new -A -s $1
 fi
-
-tmux new -A -s $1
