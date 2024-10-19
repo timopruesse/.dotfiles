307a15c027ff8e6abcc6c9d6ee0bc9f9ec6a20dc
Timo Prüße <chroma91@gmail.com>
2024-10-19 22:19:23 +0200

    chore: remove tabnine setup

--- home/.config/nvim/lua/timopruesse/init.lua
+++ home/.config/nvim/lua/timopruesse/init.lua
@@ -5,13 +5,3 @@ require("timopruesse.keymaps.init")
 require("timopruesse.autocommands.init")
 require("timopruesse.snippets.init")
 require("timopruesse.lsp")
-
-
-require("tabnine").setup({
-    disable_auto_comment = true,
-    accept_keymap = "<Tab>",
-    dismiss_keymap = "<C-]>",
-    debounce_ms = 800,
-    suggestion_color = { gui = "#808080", cterm = 244 },
-    exclude_filetypes = { "TelescopePrompt" },
-})
